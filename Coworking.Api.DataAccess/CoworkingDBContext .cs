6d6436b39ac63c96a71db8d8ab53c045d37af792
Luis Urrea <luisfernando12301985@gmail.com>
2020-04-24 18:32:45 -0500

    configurations

---
+++ Coworking.Api.DataAccess/CoworkingDBContext .cs
@@ -0,0 +1,21 @@
+﻿using Coworking.Appi.DataAccess.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAccess
+{
+    public class CoworkingDBContext : DbContext, ICoworkingDBContext
+    {
+        public CoworkingDBContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ Coworking.Appi.DataAccess.Contracts/ICoworkingDBContext .cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Appi.DataAccess.Contracts
+{
+    public interface ICoworkingDBContext
+    {
+
+    }
+}
